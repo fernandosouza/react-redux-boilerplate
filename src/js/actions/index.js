2c4be5d06e223dac021da9dcb38a6285a9d32d5a
Fernando Souza <fernando.souza@liferay.com>
2017-03-26 14:44:54 -0300

    Clean up the actions creator file

--- src/js/actions/index.js
+++ src/js/actions/index.js
@@ -1,37 +1,18 @@
+import { fetchTodos, addTodo } from '../todo';
+
 export const LOAD_TODOS = 'LOAD_TODOS';
 export const NEW_TODO = 'NEW_TODO';
 
 export function addNewTodo(todo) {
-  let set = firebase.database().ref('todo').push({
-    title: todo.title,
-    done: todo.done
-  });
-
   return {
     type: NEW_TODO,
-    payload: set.then(() => {
-      return Promise.resolve(todo);
-    })
+    payload: addTodo(todo)
   };
 }
 
 export function loadTodos() {
-  let get = firebase.database().ref('todo').once('value');
-
-  let payload = get.then(snapshot => {
-    let data = [];
-    snapshot.forEach(childSnapshot => {
-      let childData = childSnapshot.val();
-      childData.id = childSnapshot.key;
-
-      data.push(childData);
-    });
-
-    return Promise.resolve(data);
-  });
-
   return {
     type: LOAD_TODOS,
-    payload: payload
+    payload: fetchTodos()
   }
 }
--- src/js/todo/index.js
+++ src/js/todo/index.js
@@ -1,3 +1,5 @@
+const FIREBASE_DB_TODO_REF = firebase.database().ref('todo');
+
 class Todo {
   constructor(options) {
     this.title = options.title || '';
@@ -6,4 +8,32 @@ class Todo {
   }
 }
 
+function addTodo(todo) {
+  let set = FIREBASE_DB_TODO_REF.push({
+    title: todo.title,
+    done: todo.done
+  });
+
+  return set.then(() => {
+    return Promise.resolve(todo);
+  });
+}
+
+function fetchTodos() {
+  let get = FIREBASE_DB_TODO_REF.once('value');
+
+  return get.then(snapshot => {
+    let data = [];
+    snapshot.forEach(childSnapshot => {
+      let childData = childSnapshot.val();
+      childData.id = childSnapshot.key;
+
+      data.push(childData);
+    });
+
+    return Promise.resolve(data);
+  });
+}
+
 export default Todo;
+export { addTodo, fetchTodos };
